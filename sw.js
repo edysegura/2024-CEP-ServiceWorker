736de0ef946b501e76fdb38dbe029142b9f9e774
Edy Segura <edysegura@gmail.com>
2024-07-27 15:48:12 -0300

    fix: service worker implementation

--- sw.js
+++ sw.js
@@ -1,13 +1,20 @@
 self.addEventListener('install', (event) => {
-  console.log(`[Service Worker] install event lifecycle!`);
+  console.log(`[Service Worker] install event lifecycle!!`);
+  self.skipWaiting(); // don't wait for installation just activate it
   event.waitUntil(
     caches
       .open('sw-cache-v1')
       .then(async (cache) =>
-        cache.put('/images/dog.svg', await fetch('/images/cat.svg'))
+        cache.addAll([
+          '/',
+          '/index.html',
+          'https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.fluid.classless.min.css',
+          './app.js',
+          '/images/cat.svg',
+          '/images/dog.svg',
+        ])
       )
   );
-  // self.skipWaiting(); // don't wait for installation just activate it
 });
 
 self.addEventListener('activate', () => {
@@ -17,6 +24,23 @@ self.addEventListener('activate', () => {
 
 self.addEventListener('fetch', async (event) => {
   console.log(`[Service Worker] fetch event lifecycle!`);
-  const response = await caches.match(event.request.url);
-  event.respondWith(response && fetch(event.request));
+  event.respondWith(cacheFirst(event.request));
 });
+
+async function cacheFirst(request) {
+  const cache = await caches.open('sw-cache-v1');
+  const response = await cache.match(request);
+  if (response) {
+    return response;
+  }
+  console.log('URL not in the cache: ', request.url);
+  try {
+    const networkResponse = await fetch(request);
+    return networkResponse;
+  } catch (error) {
+    return new Response(`Network error happened: ${error}`, {
+      status: 408,
+      headers: { 'Content-Type': 'text/plain' },
+    });
+  }
+}
