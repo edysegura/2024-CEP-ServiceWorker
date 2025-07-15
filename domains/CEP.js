bc7bf4976a44f31a04e41bc05a12c7fe49965655
Edy Segura <edysegura@gmail.com>
2025-07-15 17:47:00 -0300

    feat: validate and clean CEP input in constructor

--- domains/CEP.js
+++ domains/CEP.js
@@ -1,5 +1,38 @@
 export default class CEP {
   constructor(cep) {
-    this.cep = cep;
+    const cleanCEP = this.#removeMask(cep);
+
+    if (!this.#isValid(cleanCEP)) {
+      throw new Error('Invalid CEP format');
+    }
+
+    this.cep = cleanCEP;
+  }
+
+  #removeMask(cep) {
+    return cep.replace(/\D/g, '');
+  }
+
+  #isValid(cep) {
+    if (!cep) return false;
+
+    // Must be exactly 8 digits after removing non-digits
+    if (cep.length !== 8) return false;
+
+    // Must contain only numbers
+    if (!/^\d+$/.test(cep)) return false;
+
+    // Cannot be all zeros
+    if (/^0{8}$/.test(cep)) return false;
+
+    return true;
+  }
+
+  toString() {
+    return this.cep.replace(/(\d{5})(\d{3})/, '$1-$2');
+  }
+
+  valueOf() {
+    return this.cep;
   }
 }
